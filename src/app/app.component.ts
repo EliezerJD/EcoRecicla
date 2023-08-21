391f2fcb5c718689355163a1677ca479d404e38a
git config --global --list <eliezerjerez1@gmail.com>
2023-08-20 22:27:53 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Inicio', url: '/folder/Inicio', icon: 'home' },
+    { title: 'Herramientas', url: '/folder/Herramientas', icon: 'accessibility' },
+    { title: 'Administrar', url: '/folder/Administrar', icon: 'build' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
